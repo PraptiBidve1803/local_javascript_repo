2971e6474d179bbd46777c09e3a1030ca6264664
PraptiBidve1803 <praptibidve18@gmail.com>
2025-11-22 01:10:18 +0530

    js basics

---
+++ 01variable.js
@@ -0,0 +1,15 @@
+console.log("Hello Prapti!");
+const AccountID =23;
+//AccountID=25 not allowed
+console.log(AccountID);
+let AccountName="Prapti";
+let AccountEmail="praptibidve18@gmail.com";
+var userCity="Nashik";
+var userCity="Pune"
+AccountEmail="praptibidve2004@gmail.com";
+console.table([AccountID,AccountName,AccountEmail,userCity]);
+
+/*There are 3 types of variable declaration in the JS
+1)const=>not redeclared or redefined ,local scope
+2)var=> redeclared and redefined , global scope
+3)let=> not redeclared but redefined/updated, local scope*/
---
+++ 02dataType.js
@@ -0,0 +1,29 @@
+
+"use strict"; //Treat All JS code as newer version
+//alert(3+3);//we are using nodejs,not browser
+
+console.log("Welcome in js");
+let Name ="Prapti"
+let age=21
+let isLoggedIn=false
+//Data types in js
+//Primitives data type
+//String => " "
+//number => 2 to power 53
+//BigInt
+//boolean => true/false
+//null => Standalone value,typeOf=object
+//undefined => not defining the any value . e.g let state;
+//symbol
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined);// undefined
+console.log(typeof age);
+console.log(typeof Name);
+
+//Assignment => look the specifications at tc39.es/ecma262(ecmascript)
+
+
+
