7955730ad32f8531b9246e5ae9850d158825472f
PraptiBidve1803 <praptibidve18@gmail.com>
2025-11-24 17:07:47 +0530

    basic of js

---
+++ 03conversionOperation.js
@@ -0,0 +1,42 @@
+let marks="33"
+console.log(typeof (marks) );
+let intoNum = Number(marks)
+console.log(marks);
+
+let condition = true
+let numCondition=Number(condition);
+console.log(typeof(numCondition));
+console.log(numCondition);
+
+let score= "abcs435"
+console.log(typeof (score));
+let Num_Score=Number(score);
+console.log(typeof Num_Score);
+console.log(Num_Score)
+
+//"33" => 33
+//"33abcd" =>NaN
+//true =>1
+//false=>0
+//1=>true
+let LogIn=1
+let booleanLogin=Boolean(LogIn)
+console.log(booleanLogin);
+
+let a=""
+let b=Boolean(a)
+console.log(b);
+let c="Prapti"
+let d=Boolean(c)
+console.log(d);
+let e=32
+let string_e=String(e)
+console.log(string_e);
+console.log(typeof string_e);
+
+
+
+
+
+
+
---
+++ 04operation.js
@@ -0,0 +1,25 @@
+let s1="Hello"
+let s2=" Prapti"
+let s3=s1+s2
+console.log(s3);
+let r=true
+let e=+true
+console.log(e);
+let x1=34
+x1++
+console.log(x1);
+--x1
+console.log(x1);
+let st1,st2,st3
+st1=st2=st3=4*4
+console.log(st2)
+console.log(st1*st2);
+console.log(st1**st2);
+console.log(st1+st2);
+console.log(st1-st2);
+console.log(st1/st2);
+console.log(st1%st2);
+
+
+
+
